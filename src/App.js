69187370ca7d5989dda8da431e88cc1af2a6a314
StuartBurmeister <stuart-burmeister@StuartBurmeisters-MacBook-Pro.local>
2020-02-11 16:40:16 +0900

    center sign in and sign up pages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import { ThemeProvider } from "@material-ui/styles";
 import { SignUp } from "./components";
 import { createMuiTheme } from '@material-ui/core';
