bf1215c17e396ec051c936fbd0866d9d77a442b7
stuart-burmeister <stuart.burmeister@vatech.co.kr>
2020-02-12 09:50:32 +0900

    vertical tab

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,27 @@
-import { Button, List, ListItem, ListItemText, Typography, makeStyles } from "@material-ui/core";
-import React from "react";
+import { Button, List, ListItem, ListItemText, Typography, makeStyles, Tab, AppBar, Tabs } from "@material-ui/core";
+import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
+import App from "../../App";
+import { withStyles } from "@material-ui/styles";
+
+const VerticalTabs = withStyles(theme => ({
+  flexContainer: {
+    flexDirection: 'column'
+  },
+  indicator: {
+    display: 'none',
+  }
+}))(Tabs);
+
+const MyTab = withStyles(theme => ({
+  selected: {
+    color: 'tomato',
+    borderBottom: '2px solid tomato'
+  }
+}))(Tab);
 
 const useStyles = makeStyles(() => ({
-  root: { height: "100vh", position: "relative", },
-  container: { flexDirection: "column", height: "100%", alignItems: "center", justifyContent: "center" },
+  container: { flexDirection: "column", height: "100%", width:"10%", alignItems: "center", justifyContent: "center" },
   item: { width: 380, },
   header: { textAlign: "center", color: "#00897b", fontSize: 24, fontWeight: "bold" },
   input: { width: "100%", fontSize: 14 },
@@ -18,22 +35,17 @@ const NavBar = props => {
   const tabs = [{ title: "Main", link: "links[0]" },
   { title: "User", link: "links[1]" },
   { title: "Message", link: "links[2]" },];
+  const [value, setValue] = useState(0);
   //const history = useHistory();
   return (
-    <List component="nav" style={{height:"100vh"}}>
-      <ListItem component="div" style={{flexDirection:"column", backgroundColor:"#00897b", width:"10%", height:"100vh"}}>
-        {tabs.map(({ title, link }) => {
-          return (
-            <Button className={classes.button} fullWidth color="primary">
-
-                  {title}
-
-            </Button>
-          )
-        }
-        )}
-      </ListItem>
-    </List>
+      <VerticalTabs
+        value={value}
+        onChange={(_, newValue) => setValue(newValue)}
+        color="primary">
+        <MyTab label="Main"/>
+        <MyTab label="User"/>
+        <MyTab label="Message"/>
+      </VerticalTabs>
   );
 }
 
