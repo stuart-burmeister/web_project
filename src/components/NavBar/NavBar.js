43dbd32c57bfcf3968b181b08ed4b8b3ca91ffe8
stuart-burmeister <stuart.burmeister@vatech.co.kr>
2020-02-12 10:44:27 +0900

    Tab styling

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,24 +5,26 @@ import App from "../../App";
 import { withStyles } from "@material-ui/styles";
 
 const VerticalTabs = withStyles(theme => ({
-  flexContainer: {
-    flexDirection: 'column'
-  },
   indicator: {
     display: 'none',
+  },
+  flexContainer: {
+    display: "grid"
   }
+
 }))(Tabs);
 
-const MyTab = withStyles(theme => ({
+const MenuTab = withStyles(theme => ({
   selected: {
-    color: 'tomato',
-    borderBottom: '2px solid tomato'
+    color: "black",
+    backgroundColor: "white",
+    display: "grid"
   }
 }))(Tab);
 
 const useStyles = makeStyles(() => ({
-  container: { flexDirection: "column", height: "100%", width:"10%", alignItems: "center", justifyContent: "center" },
-  item: { width: 380, },
+  container: { flexDirection: "column", height: "100vh", width: "80px", alignItems: "center", justifyContent: "flex-start" },
+  item: { width: 80, height: 80, fontSize: 14, fontWeight: "bold" },
   header: { textAlign: "center", color: "#00897b", fontSize: 24, fontWeight: "bold" },
   input: { width: "100%", fontSize: 14 },
   button: { color: "#ffffff", fontSize: 14 },
@@ -38,14 +40,16 @@ const NavBar = props => {
   const [value, setValue] = useState(0);
   //const history = useHistory();
   return (
+    <AppBar className={classes.container} position="static">
       <VerticalTabs
+        orientation="vertical"
         value={value}
-        onChange={(_, newValue) => setValue(newValue)}
-        color="primary">
-        <MyTab label="Main"/>
-        <MyTab label="User"/>
-        <MyTab label="Message"/>
+        onChange={(_, newValue) => setValue(newValue)}>
+        <MenuTab disableRipple className={classes.item} label="Main" />
+        <MenuTab disableRipple className={classes.item} label="User" />
+        <MenuTab disableRipple className={classes.item} label="Message" />
       </VerticalTabs>
+    </AppBar>
   );
 }
 
