ef711291f61273811830ed1f0102131d8d6b358f
stuart-burmeister <stuart.burmeister@vatech.co.kr>
2020-02-12 15:11:50 +0900

    refactor styles

--- src/components/MainPage/UserPanel/UserPanel.js
+++ src/components/MainPage/UserPanel/UserPanel.js
@@ -1,13 +1,22 @@
 import React, { useState } from "react";
-import { Grid } from "@material-ui/core";
+import { Grid, Box, makeStyles } from "@material-ui/core";
 import { SearchBar } from "../..";
 
+const useStyle = makeStyles(() => ({
+  root: { width: "100%", },
+  item: { width: "100%", },
+  box: { padding: 20, height: "100%" },
+}))
+
 const UserPanel = () => {
-  const [userFilter, setUserFilter] = useState ("");
+  const [userFilter, setUserFilter] = useState("");
+  const classes = useStyle();
   return (
-    <Grid container style={{width:"100%"}}>
-      <Grid item style={{width:"100%"}}>
-        <SearchBar setFilter={(filter) => setUserFilter(filter)}/>
+    <Grid className={classes.root} container>
+      <Grid className={classes.item} item>
+        <Box item className={classes.box} border={2} borderColor={"#979797"}>
+          <SearchBar setFilter={(filter) => setUserFilter(filter)} />
+        </Box>
       </Grid>
     </Grid>
   );
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,17 +1,23 @@
-import React, { useState } from "react";
-import { Card, TextField, Button, SvgIcon } from "@material-ui/core";
+import { Button, makeStyles, TextField } from "@material-ui/core";
 import SearchIcon from '@material-ui/icons/Search';
+import React, { useState } from "react";
+
+const useStyles = makeStyles(() => ({
+  root: { width: "100%", height: "100%", justifyContent: 'center', },
+  inputField: { width: "65%" },
+  button: { width: "10%", height: "100%", color: "white", backgroundColor: "#c8c8c8", marginLeft: 10 }
+}));
 
 const SearchBar = (props) => {
   const { setFilter } = props;
   const [inputVal, setInputVal] = useState("");
-
+  const classes = useStyles();
   return (
-    <div style={{width:"100%",height:"100%"}}>
-      <TextField style={{width:"65%"}} variant="outlined" label="Search" onChange={({target}) => setInputVal(target.value)} value={inputVal}>
+    <div className={classes.root}>
+      <TextField className={classes.inputField} variant="outlined" label="Search" onChange={({ target }) => setInputVal(target.value)} value={inputVal}>
       </TextField>
-      <Button style={{width:"10%",height:"100%", color:"white", backgroundColor:"#c8c8c8", marginLeft:10}} onClick={() => setFilter(inputVal)}>
-        <SearchIcon fontSize="large"/>
+      <Button className={classes.button} onClick={() => setFilter(inputVal)}>
+        <SearchIcon fontSize="large" />
       </Button>
     </div>
   );
