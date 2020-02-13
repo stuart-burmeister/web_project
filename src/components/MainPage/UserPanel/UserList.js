6494c119a45717ce0d223e545e89d187c28d912c
stuart-burmeister <stuart.burmeister@vatech.co.kr>
2020-02-13 09:28:10 +0900

    user list layout fixes

--- src/components/MainPage/UserPanel/UserList.js
+++ src/components/MainPage/UserPanel/UserList.js
@@ -1,20 +1,20 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
+import { Typography, Box } from "@material-ui/core";
 
 const UserList = props => {
   const { filter } = props;
   return (
     <div style={{ display: "flex", height: "100%", width: "100%", borderColor: "#979797", flexDirection: "column", }}>
-      <div style={{ display: "flex", height: 48, flexDirection: "row", alignItems: "center", }}>
-        <Typography style={{ width: 253, paddingLeft: 23, fontWeight: "bold" }}>
+      <Box borderBottom={1} color={"#979797"} style={{ display: "flex", height: 48, flexDirection: "row", alignItems: "center", }}>
+        <Typography style={{ width: 253, paddingLeft: 23, fontWeight: "bold", color: "black" }}>
           EMAIL
         </Typography>
-        <Typography style={{ fontWeight: "bold" }}>
+        <Typography style={{ fontWeight: "bold" , color: "black"}}>
           NAME
         </Typography>
-      </div>
+      </Box>
       <div style={{ flex: 1, flexDirection: "column", }}>
-
+        
       </div>
     </div>
   );
