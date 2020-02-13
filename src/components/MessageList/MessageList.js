f4652d26c73b49d42e625c993f917032d7ffbe4d
stuart-burmeister <stuart.burmeister@vatech.co.kr>
2020-02-13 16:44:25 +0900

    delete icon

--- src/components/MessageList/MessageList.js
+++ src/components/MessageList/MessageList.js
@@ -1,9 +1,15 @@
-import { makeStyles, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@material-ui/core";
+import { IconButton, makeStyles, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@material-ui/core";
 import React from "react";
 
-const useStyles =  makeStyles(() => ({
+const useStyles = makeStyles(() => ({
   root: { display: "flex", height: "100%", width: "100%", flexDirection: "column", },
   container: { maxHeight: props => props.maxHeight },
+  icon: {
+    width:20,
+    backgroundColor: "white",
+    borderWidth: 1,
+    borderColor: "#979797"
+  },
   headMail: {
     maxWidth: 250,
     fontWeight: "bold",
@@ -19,10 +25,19 @@ const useStyles =  makeStyles(() => ({
   },
   oddRow: { backgroundColor: "white", fontWeight: "bold", fontSize: 14 },
   evenRow: { backgroundColor: "#979797", fontWeight: "bold", fontSize: 14 },
+  oval: {
+    display:"flex",
+    flexDirection:"column",
+    width: "16px",
+    height: "16px",
+    backgroundColor: "#979797",
+    fontSize:14,
+    color: "white",
+  },
 }));
 
 const MessageList = props => {
-  const { messages} = props;
+  const { messages } = props;
   const classes = useStyles(props);
   return (
     <div className={classes.root}>
@@ -30,6 +45,7 @@ const MessageList = props => {
         <Table stickyHeader>
           <TableHead >
             <TableRow>
+              <TableCell className={classes.icon} />
               <TableCell className={classes.headMail}>
                 DATE
               </TableCell>
@@ -44,6 +60,13 @@ const MessageList = props => {
                 const rowStyle = index % 2 ? classes.evenRow : classes.oddRow;
                 return (
                   <TableRow>
+                    <TableCell className={rowStyle}>
+                      <IconButton className={classes.oval} >
+                        <div >
+                          X
+                        </div>
+                      </IconButton>
+                    </TableCell>
                     <TableCell className={rowStyle}>
                       {row.date}
                     </TableCell>
