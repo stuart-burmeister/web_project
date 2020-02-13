2830310123c91121b87c0466f39db60d538a2952
stuart-burmeister <stuart.burmeister@vatech.co.kr>
2020-02-13 10:38:49 +0900

    user list  table

--- src/components/MainPage/UserPanel/UserList.js
+++ src/components/MainPage/UserPanel/UserList.js
@@ -1,21 +1,69 @@
+import { makeStyles, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@material-ui/core";
 import React from "react";
-import { Typography, Box } from "@material-ui/core";
+
+const useStyles = makeStyles(() => ({
+  head: {
+    backgroundColor: "white",
+    borderWidth: 5
+  },
+  body: {
+    fontSize: 14
+  }
+}));
+
+const dummyValues = [
+  { name: "erin", email: "friend@sis.com" },
+  { name: "His Lordship Montgomery Ericcson Lancaster III", email: "superboy@play.co.za" },
+  { name: "pete", email: "friend@bro.com" },
+  { name: "moosh", email: "snooze@lazy.co.uk" },
+  { name: "erin", email: "friend@sis.com" },
+  { name: "His Lordship Montgomery Ericcson Lancaster III", email: "superboy@play.co.za" },
+  { name: "pete", email: "friend@bro.com" },
+  { name: "moosh", email: "snooze@lazy.co.uk" },
+  { name: "erin", email: "friend@sis.com" },
+  { name: "His Lordship Montgomery Ericcson Lancaster III", email: "superboy@play.co.za" },
+  { name: "pete", email: "friend@bro.com" },
+  { name: "moosh", email: "snooze@lazy.co.uk" },
+  { name: "erin", email: "friend@sis.com" },
+  { name: "His Lordship Montgomery Ericcson Lancaster III", email: "superboy@play.co.za" },
+  { name: "pete", email: "friend@bro.com" },
+  { name: "moosh", email: "snooze@lazy.co.uk" },
+];
 
 const UserList = props => {
   const { filter } = props;
+  const classes = useStyles();
+  const users = dummyValues;
   return (
-    <div style={{ display: "flex", height: "100%", width: "100%", borderColor: "#979797", flexDirection: "column", }}>
-      <Box borderBottom={1} color={"#979797"} style={{ display: "flex", height: 48, flexDirection: "row", alignItems: "center", }}>
-        <Typography style={{ width: 253, paddingLeft: 23, fontWeight: "bold", color: "black" }}>
-          EMAIL
-        </Typography>
-        <Typography style={{ fontWeight: "bold" , color: "black"}}>
-          NAME
-        </Typography>
-      </Box>
-      <div style={{ flex: 1, flexDirection: "column", }}>
-        
-      </div>
+    <div style={{ display: "flex", height: "100%", width: "100%", flexDirection: "column", }}>
+      <TableContainer style={{ maxHeight: 630 }}>
+        <Table stickyHeader>
+          <TableHead >
+            <TableRow style={{ backgroundColor: "#979797" }}>
+              <TableCell style={{ width: 250, fontWeight: "bold", backgroundColor: "white", borderWidth: 1, borderColor: "#979797" }}>
+                EMAIL
+              </TableCell>
+              <TableCell style={{ fontWeight: "bold", backgroundColor: "white", borderWidth: 1, borderColor: "#979797" }}>
+                NAME
+              </TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {
+              users.map((row, index) => (
+                <TableRow>
+                  <TableCell style={{ backgroundColor: (index % 2 === 0) ? "white" : "#979797", fontWeight: "bold" }}>
+                    {row.email}
+                  </TableCell>
+                  <TableCell style={{ backgroundColor: (index % 2 === 0) ? "white" : "#979797", fontWeight: "bold" }}>
+                    {row.name}
+                  </TableCell>
+                </TableRow>
+              ))
+            }
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 };
