25a896564edc9a817b917a85600426e25eeb6776
stuart-burmeister <stuart.burmeister@vatech.co.kr>
2020-02-19 15:59:15 +0900

    style optimise MainPage

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,16 +1,23 @@
 import { Box, Grid, makeStyles, Typography } from "@material-ui/core";
+import clsx from "clsx";
 import React, { useState } from "react";
-import { MessagePanel } from "./components/";
 import { UserPanel } from "../";
+import { MessagePanel } from "./components/";
+
 
 const useStyle = makeStyles(() => ({
-  root: { display: "flex", width: "100%", height: "100%", flexDirection: "row" },
+  root: {
+    display: "flex",
+    width: "100%",
+    height: "100%",
+  },
   user__panel: { flex: 1 },
   box: { height: "100%", },
   message__panel: { flex: 2 },
   empty__panel: {
-    display: "flex", height: "100%", width: "100%",
-    alignItems: "center", justifyContent: "center", backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "black",
   },
   message: { fontWeight: "bold" }
 }));
@@ -18,30 +25,31 @@ const useStyle = makeStyles(() => ({
 const MainPage = () => {
   const classes = useStyle();
   const [currentUser, setCurrentUser] = useState(null);
-  
-  return (
-    <Grid className={classes.root} container spacing={3}>
-      <Grid className={classes.user__panel} item>
-        <Box className={classes.box} border={1} borderColor={"#979797"}>
-          <UserPanel selectedUser={currentUser} onSelect={(newUser) => setCurrentUser(newUser)} />
-        </Box>
-      </Grid>
-      <Grid className={classes.message__panel} item>
-        <Box className={classes.box} border={1} borderColor={"#979797"}>
-          {
-            currentUser ?
-              <MessagePanel title email={currentUser.email} /> :
-              <Box className={classes.empty__panel}>
-                <Typography className={classes.message} color="primary">
-                  Select an item on the left.
-              </Typography>
-              </Box>
-          }
-        </Box>
-      </Grid>
-    </Grid>
-  );
 
+  return (
+    <Box className={classes.root}>
+      <Grid className={classes.root} container spacing={3}>
+        <Grid className={classes.user__panel} item>
+          <Box className={classes.box} border={1} borderColor={"#979797"}>
+            <UserPanel selectedUser={currentUser} onSelect={(newUser) => setCurrentUser(newUser)} />
+          </Box>
+        </Grid>
+        <Grid className={classes.message__panel} item>
+          <Box className={classes.box} border={1} borderColor={"#979797"}>
+            {
+              currentUser ?
+                <MessagePanel title email={currentUser.email} /> :
+                <Box className={clsx(classes.root, classes.empty__panel)}>
+                  <Typography className={classes.message} color="primary">
+                    Select an item on the left.
+                </Typography>
+                </Box>
+            }
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export default MainPage;
