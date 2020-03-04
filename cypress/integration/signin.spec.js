4b21e08470faff2001709edd78e5409f8276418f
stuart-burmeister <stuart.burmeister@vatech.co.kr>
2020-03-04 17:22:28 +0900

    check stored data

--- cypress/integration/signin.spec.js
+++ cypress/integration/signin.spec.js
@@ -15,13 +15,16 @@ describe('The Signin Page', function() {
     // test signup
     cy.get("#email").type("test")
     cy.get("#password").type("test")
-    cy.get("#signin").click()
-
-    cy.url().should('include', '/main')
+    cy.get("#signin").click().should(() => 
+      expect(sessionStorage.getItem('currentUser')).to.eq('test')
+    )
   })
 
   it('successfully logs out', function() {
-    cy.contains("Logout").click()
+    cy.url().should('include', '/main')
+    cy.contains("Logout").click().should(() => 
+    expect(sessionStorage.getItem('currentUser')).to.eq(null)
+  )
     cy.url().should('include', '/signin')
   })    
 
